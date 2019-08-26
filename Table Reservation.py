9ae4959242c2452878f96ab5bd4f7dac79ac3b83
aphan21 <54091788+aphan21@users.noreply.github.com>
2019-08-26 11:37:29 +0700

    Update Table Reservation.py

--- Table Reservation.py
+++ Table Reservation.py
@@ -1,4 +1,16 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+#This program checks if a person is in the reservation list in a restaurant
+#You must be in the reservation list to get a table 
+
+name = input ("Welcome! What is your name?")
+
+reservation_name = ["Ann","Claire","Steven","Jason"]
+if name == reservation_name[0]:
+    print("Right this way!")
+elif name == reservation_name[1]:
+    print("Right this way!")
+elif name == reservation_name[2]:
+    print("Right this way!")
+elif name == reservation_name[3]:
+    print("Right this way!")
+else:
+    print("Sorry, we don't have a reservation under that name.")
